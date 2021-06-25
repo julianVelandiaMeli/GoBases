fddaa4a3ad9e19c166ca0000d54cbc3d5987e021
julianVelandiaMeli <julian.velandia@mercadolibre.com.co>
2021-06-25 11:29:05 -0500

    ejercicio 2 funcionando

--- Clase1/ejercicio2.go
+++ Clase1/ejercicio2.go
@@ -7,6 +7,17 @@ import (
 
 func main() {
 
-	fmt.Println("Hola mundo 2") //Imprimir
+	var temperatura float32 //Medido en Centígrados
+	var humedad int16       //Medidos en porcentaje
+	var presion float32     //Medidos en atmósferas
+
+	temperatura = 17.5
+	humedad = 52
+	presion = 1.1
+
+	fmt.Println("Para la ciudad de Bogotá, se tiene los siguentes datos: ")
+	fmt.Println("La temperatura es de ", temperatura, " grados centigrados")
+	fmt.Println("La humedad es de ", humedad, " porciento")
+	fmt.Println("La presion atmosférica es de ", presion, " atmósferas")
 
 }
